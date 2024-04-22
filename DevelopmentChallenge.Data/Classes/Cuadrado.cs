e37841b6a167bac05c52b5ef66862e35b336a00d
Elías Álvarez <ealvarez@awg.la>
2024-04-22 05:18:07 -0300

    add remove using

--- DevelopmentChallenge.Data/Classes/Cuadrado.cs
+++ DevelopmentChallenge.Data/Classes/Cuadrado.cs
@@ -1,10 +1,4 @@
 ﻿using DevelopmentChallenge.Data.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevelopmentChallenge.Data.Classes
 {
--- DevelopmentChallenge.Data/Classes/TrianguloEquilatero.cs
+++ DevelopmentChallenge.Data/Classes/TrianguloEquilatero.cs
@@ -1,9 +1,5 @@
 ﻿using DevelopmentChallenge.Data.Enums;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevelopmentChallenge.Data.Classes
 {
